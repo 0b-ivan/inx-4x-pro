fc0d50e98d6c17ea4b229ed42e6873e44b892a77
Ob-Ivan <55740240+0b-ivan@users.noreply.github.com>
2026-08-31 00:45:34 +0200

    feat: persist X4 Pro quick-control state

---
+++ src/system/X4ProQuickPrefs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class X4ProQuickPrefs {
+ public:
+  static bool nightMode();
+  static void setNightMode(bool enabled);
+
+  static bool readerTouchEnabled();
+  static void setReaderTouchEnabled(bool enabled);
+};
