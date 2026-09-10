5f0626206855fc10ad5519d08c0ca8eab4816f6d
Ob-Ivan <55740240+0b-ivan@users.noreply.github.com>
2026-09-10 22:08:14 +0200

    feat: identify X4 player to browser battleship

--- src/apps_local/battleship/web/BattleshipLocalPlayActivity.h
+++ src/apps_local/battleship/web/BattleshipLocalPlayActivity.h
@@ -2,6 +2,7 @@
 
 #include "../../../activities/Activity.h"
 #include "../../../components/UITheme.h"
+#include "../../player/PlayerName.h"
 #include "../../ui/ToyboxScreen.h"
 #include "../BattleshipCore.h"
 #include "BattleshipBrowserServer.h"
@@ -10,7 +11,11 @@
 class BattleshipLocalPlayActivity final : public Activity {
  public:
   BattleshipLocalPlayActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
-      : Activity("BattleshipLocalPlay", renderer, mappedInput) {}
+      : Activity("BattleshipLocalPlay", renderer, mappedInput) {
+    // The reader already has one device-wide Player identity. Reuse it for the
+    // browser opponent instead of presenting the hardware model as a player.
+    browser_.setOpponentName(player::name());
+  }
 
   void onEnter() override;
   void onExit() override;
