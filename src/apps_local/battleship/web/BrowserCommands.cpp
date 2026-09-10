2cf6abc60aa6f476c2e35b02520918280f04f4a0
Ob-Ivan <55740240+0b-ivan@users.noreply.github.com>
2026-09-10 17:53:40 +0200

    feat: parse browser surrender command

--- src/apps_local/battleship/web/BrowserCommands.cpp
+++ src/apps_local/battleship/web/BrowserCommands.cpp
@@ -77,6 +77,8 @@ bool parseCommand(const uint8_t* bytes, size_t size, Command& out) {
     candidate.kind = CommandKind::Ready;
   else if (r.literal("\"fire\""))
     candidate.kind = CommandKind::Fire;
+  else if (r.literal("\"surrender\""))
+    candidate.kind = CommandKind::Surrender;
   else if (r.literal("\"rematch\""))
     candidate.kind = CommandKind::Rematch;
   else if (r.literal("\"resume\""))
