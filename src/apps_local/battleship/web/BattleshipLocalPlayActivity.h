3d765246717287169562b472a4a46dc0256958c2
Ob-Ivan <55740240+0b-ivan@users.noreply.github.com>
2026-09-10 11:43:15 +0200

    feat: transition browser ready to X4 placement

--- src/apps_local/battleship/web/BattleshipLocalPlayActivity.h
+++ src/apps_local/battleship/web/BattleshipLocalPlayActivity.h
@@ -22,20 +22,44 @@ class BattleshipLocalPlayActivity final : public Activity {
   bool preventAutoSleep() override { return browser_.running(); }
 
  private:
-  enum class Stage : uint8_t { Transport, Network, BrowserWaiting };
+  enum class Stage : uint8_t { Transport, Network, BrowserWaiting, X4Placement, Playing };
+
+  struct GridGeometry {
+    int originX = 0;
+    int originY = 0;
+    int cell = 0;
+  };
 
   void routeChoiceInput();
+  void routeX4PlacementInput();
   void chooseTransport(int index);
   void chooseNetwork(int index);
   void startWifiBrowser();
   void startHotspotBrowser();
   void enterBrowserWaiting();
+  void startX4Placement();
+  void commitX4Fleet();
   void cleanupBrowserNetwork();
   void finishCancelled();
+
+  GridGeometry x4PlaceGrid() const;
+  static int cellAt(const GridGeometry& grid, int x, int y);
+  static Rect cellRect(const GridGeometry& grid, int cell);
+  Rect x4PlaceRosterRect() const;
+  int x4PlaceRosterRowAt(int x, int y) const;
+  void selectX4Ship(int shipIndex);
+  void rotateX4Ship();
+  void moveX4Ship(int cell);
+  void handleX4PlaceTap(int cell);
+  void shuffleX4Fleet();
 
   void drawTransportChoice();
   void drawNetworkChoice();
   void drawBrowserWaiting();
+  void drawX4Placement();
+  void drawPlaying();
+  void drawX4PlaceGrid();
+  void drawX4PlaceRoster();
 
   Stage stage_ = Stage::Transport;
   toybox::Interactions interactions_;
@@ -44,7 +68,12 @@ class BattleshipLocalPlayActivity final : public Activity {
 
   bshipweb::BrowserPlayer browserPlayer_;
   bship::Game browserGame_;
+  bship::Fleet x4Fleet_;
   bshipweb::Server browser_;
+  Rect x4BodySlot_{};
+  uint32_t seed_ = 1;
+  int selectedX4Ship_ = -1;
+  char x4Status_[48] = {};
   bool devModePaused_ = false;
   bool ownsSta_ = false;
   bool lastClientSeen_ = false;
